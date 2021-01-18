e2f259d4cfb218aa87cb66f70c04896840dd9fed
Trend20 <enockomondi305@gmail.com>
2021-01-18 12:41:56 +0300

    fetching a random image

--- app.js
+++ app.js
@@ -1,21 +1,50 @@
-// DOM CONTENT
-const findBtn = document.getElementById('find');
+// // DOM CONTENT
+// const findBtn = document.getElementById('find');
 
-const dogContainer = document.getElementById('dogs');
+// const dogContainer = document.getElementById('dogs');
 
-// ADDING EVENTS
+// // ADDING EVENTS
 
-findBtn.addEventListener('click', getApi);
+// findBtn.addEventListener('click', getApi);
 
-// API
-let apiUrl = 'https://dog.ceo/api/breeds/image/random';
+// // API
+// let apiUrl = 'https://dog.ceo/api/breeds/image/random';
 
 
-async function getApi(url) {
-    // Storing response 
-    const response = await fetch(apiUrl);
+// async function getApi(url) {
+//     // Storing response 
+//     const response = await fetch(apiUrl);
 
-    // Storing data in form of JSON 
-    var data = await response.json();
-    console.log(data);
+//     // Storing data in form of JSON 
+//     var data = await response.json();
+//     console.log(data);
+// }
+
+const dogImage = document.getElementById('dogImage');
+
+
+
+// get random dog image
+
+function getRandomImage() {
+
+    let dogImageUrl = 'https://dog.ceo/api/breeds/image/random';
+
+
+    // fetch the random image
+
+    fetch(dogImageUrl)
+        .then((response) => {
+            return response.json();
+        })
+        .then((json) => {
+            console.log(json);
+            let imageUrl = json.message;
+            dogImage.src = imageUrl;
+        })
+        .catch((error) => {
+            console.log(error)
+        })
 }
+
+getRandomImage();
